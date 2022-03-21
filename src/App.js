51a5d0cd20363c54c54bd087c827852a58ba0bd4
usrwatermelon <kyntltntn@protonmail.com>
2022-03-21 19:51:49 +0000

    Fuse MainPage into App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { Outlet } from "react-router-dom";
-import MainPage from "./components/MainPage";
+import Laptop from "./assets/Laptop.jpeg";
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
-      <Outlet />
+      <div className="home">
+        <div className="about">
+          <div className="about-desc">
+            <h1 className="about-desc-heading">
+              Get the latest tech for the best price.
+            </h1>
+            <p className="about-desc-p">
+              The best prices for the most modern technology offered today. Make
+              the best out of the best laptops, mobile phones, PC parts, and
+              more.{" "}
+            </p>
+          </div>
+          <img src={Laptop} alt="Laptop" className="about-img" />
+        </div>
+      </div>
     </div>
   );
 }
