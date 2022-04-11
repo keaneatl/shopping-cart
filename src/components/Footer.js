fdb1bd916b90d9aa306b250fa3233a6be818773f
usrwatermelon <kyntltntn@protonmail.com>
2022-04-11 12:22:54 +0000

    Fix github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ import { FaGithub } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer>
-      <a href="github.com/keaneatl/shopping-cart" target="_blank">
+      <a
+        href="github.com/keaneatl/shopping-cart"
+        target="_blank"
+        rel="noopener"
+      >
         <FaGithub /> Made by Keane Andre
       </a>
     </footer>
