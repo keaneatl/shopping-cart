00c95d95ab34e0c414bb40d6b4a7862a06212bc8
usrwatermelon <kyntltntn@protonmail.com>
2022-04-11 12:15:26 +0000

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import About from "./assets/images/about.jpeg";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="home">
-      <div className="about-desc">
-        <h1 className="about-desc-heading">
-          Get the latest tech for the best price.
-        </h1>
-        <p className="about-desc-p">
-          The best prices for the most modern technology offered today. Make the
-          best out of the best laptops, mobile phones, PC parts, and more.{" "}
-        </p>
+    <div className="app">
+      <div className="home">
+        <div className="about-desc">
+          <h1 className="about-desc-heading">
+            Get the latest tech for the best price.
+          </h1>
+          <p className="about-desc-p">
+            The best prices for the most modern technology offered today. Make
+            the best out of the best laptops, mobile phones, PC parts, and more.{" "}
+          </p>
+        </div>
+        <img src={About} alt="Laptop" className="about-img" />
       </div>
-      <img src={About} alt="Laptop" className="about-img" />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import { FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="github.com/keaneatl/shopping-cart" target="_blank">
+        <FaGithub /> Made by Keane Andre
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
